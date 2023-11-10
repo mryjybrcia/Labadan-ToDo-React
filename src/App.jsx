f8d0c62e14f83370ae09c6fd6cf759a9d449d26a
Mary Joy Labadan <141330072+mryjybrcia@users.noreply.github.com>
2023-11-10 08:54:29 +0800

    Updated design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
-import Center from "./components/Center";
+import { useDispatch, useSelector } from "react-redux";
+import Home from "./components/Home";
+import EmptyBoard from "./components/EmptyBoard";
+import boardSlice from "./redux/boardSlice";
 
 function App() {
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardSlice.actions.setBoardActive({ index: 0 }));
   return (
-    <div>
-      <Header />
-
-      <Center />
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <>
+        {boards.length > 0 ? (
+          <>
+            <Header
+              setIsBoardModalOpen={setIsBoardModalOpen}
+              isBoardModalOpen={isBoardModalOpen}
+            />
+            <Home
+              setIsBoardModalOpen={setIsBoardModalOpen}
+              isBoardModalOpen={isBoardModalOpen}
+            />
+          </>
+        ) : (
+          <>
+            <EmptyBoard type="add" />
+          </>
+        )}
+      </>
     </div>
   );
 }
--- src/components/Center.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Center() {
-  return <div>Center</div>;
-}
-
-export default Center;
